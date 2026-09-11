bffa272460b32c019b36c7dc31a93a58fcca0a59
JPipowski <jpipowski@mail.roosevelt.edu>
2026-09-11 11:34:44 -0500

    I added code to display the Molecular Weight and Forumula for Theobromine as well as SMILES. The second code addes a correction so that the SMILES string is correctly displayed.

--- PubChemFetcher/PubChemFetcher.py
+++ PubChemFetcher/PubChemFetcher.py
@@ -1,11 +1,11 @@
 import pubchempy as pcp
 
-properties = pcp.get_properties(['MolecularFormula', 'MolecularWeight', 'CanonicalSMILES'], 'theobromine', 'name')
+compounds = pcp.get_compounds('theobromine', 'name')
 
-if properties:
-    compound_props = properties[0]
-    print(f"Molecular Formula: {compound_props.get('MolecularFormula')}")
-    print(f"Molecular Weight: {compound_props.get('MolecularWeight')}")
-    print(f"SMILES String: {compound_props.get('CanonicalSMILES')}")
+if compounds:
+    compound = compounds[0]
+    print(f"Molecular Formula: {compound.molecular_formula}")
+    print(f"Molecular Weight: {compound.molecular_weight}")
+    print(f"SMILES String: {compound.canonical_smiles}")
 else:
     print("Compound not found.")
