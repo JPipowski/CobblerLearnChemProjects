4a3449a12d0eb1404e0eefe083d1db0d37ca473f
JPipowski <jpipowski@mail.roosevelt.edu>
2026-09-11 11:44:50 -0500

    I added code to display the Molecular Weight and Forumula for Theobromine as well as SMILES. The second code addes a correction so that the SMILES string is correctly displayed.
    The third code allows you to imput any compound and receive its molecular weight, formula, and SMILES string.

--- PubChemFetcher/PubChemFetcher.py
+++ PubChemFetcher/PubChemFetcher.py
@@ -6,8 +6,10 @@ compounds = pcp.get_compounds(compound_name, 'name')
 if compounds:
     compound = compounds[0]
     print(f"\nCompound: {compound_name.title()}")
+    print(f"IUPAC Name: {compound.iupac_name}")
     print(f"Molecular Formula: {compound.molecular_formula}")
     print(f"Molecular Weight: {compound.molecular_weight}")
     print(f"SMILES String: {compound.connectivity_smiles}")
+
 else:
     print(f"Compound '{compound_name}' not found.")
